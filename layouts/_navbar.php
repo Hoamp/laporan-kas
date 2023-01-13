9fc9321acc47c7bdcbfcc7eecca99373f422d092
IbuknyaHoamp <sukarnis243@gmail.com>
2023-01-13 08:28:46 +0700

    penambahan template

---
+++ layouts/_navbar.php
@@ -0,0 +1,36 @@
+<!-- partial:partials/_navbar.html -->
+<nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+    <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo" href="index.html"><img src="images/logo.svg" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="images/logo-mini.svg" alt="logo"/></a>
+        <button class="navbar-toggler navbar-toggler align-self-center d-none d-lg-flex" type="button" data-toggle="minimize">
+            <span class="typcn typcn-th-menu"></span>
+        </button>
+    </div>
+    <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <ul class="navbar-nav mr-lg-2">
+            <li class="nav-item  d-none d-lg-flex">
+                <a class="nav-link active" href="#">
+                Main
+                </a>
+            </li>           
+        </ul>
+        <ul class="navbar-nav navbar-nav-right">
+            <li class="nav-item nav-profile dropdown">
+                <a class="nav-link dropdown-toggle  pl-0 pr-0" href="#" data-toggle="dropdown" id="profileDropdown">
+                    <i class="typcn typcn-user-outline mr-0"></i>
+                    <span class="nav-profile-name">Thomas Setiawan</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+                    <a class="dropdown-item">
+                    <i class="typcn typcn-power text-primary"></i>
+                    Logout
+                    </a>
+                </div>
+            </li>
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+            <span class="typcn typcn-th-menu"></span>
+        </button>
+    </div>
+</nav>
---
+++ layouts/_sidebar.php
@@ -0,0 +1,4 @@
+<!-- partial -->
+<div class="container-fluid page-body-wrapper">
+<?php require_once 'layouts/partials/aside.php'; ?>
+</div>
